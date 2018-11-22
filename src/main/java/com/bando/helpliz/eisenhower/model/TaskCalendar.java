3c4a33117412df6eefd765a23766c70a66ddd7be
Balazs Ando <elnesia@gmail.com>
2018-11-22 22:31:05 +0100

    finetuning

--- src/main/java/com/bando/helpliz/eisenhower/model/TaskCalendar.java
+++ src/main/java/com/bando/helpliz/eisenhower/model/TaskCalendar.java
@@ -11,8 +11,15 @@ public class TaskCalendar extends ArrayList<TaskEntry>
         this.add(new TaskEntry(date, task));
     }
 
+    @Override public boolean add(TaskEntry taskEntry)
+    {
+        boolean ret = super.add(taskEntry);
+        if(ret) this.sort(TaskEntry::compareTo);
+        return ret;
+    }
+
     @Override public String toString()
     {
-        return Arrays.toString(this.stream().sorted().toArray());
+        return Arrays.toString(this.toArray());
     }
 }
